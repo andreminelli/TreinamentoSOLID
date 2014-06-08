1516a5da04910381bce330475cbeb0ca2302bcab
André Minelli <andre@takenet.com.br>
2014-06-08 18:30:47 -0300

    Removed unused 'usings'

--- Src/SOLID/SOLID.ETL/Program.cs
+++ Src/SOLID/SOLID.ETL/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Data.SqlServerCe;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SOLID.ETL
 {
