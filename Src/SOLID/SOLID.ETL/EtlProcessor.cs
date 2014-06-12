db949a9c29a473b8682b0043509c6867ffe7e53e
André Minelli <andre@takenet.com.br>
2014-06-12 09:05:25 -0300

    Extracted behavior from Program to EtlProcessor class

---
+++ Src/SOLID/SOLID.ETL/EtlProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SOLID.ETL
+{
+    public class EtlProcessor
+    {
+        private string _sourceFilePath;
+        private string _targetConnectionString;
+
+        public EtlProcessor(string sourceFilePath, string targetConnectionString)
+        {
+            this._sourceFilePath = sourceFilePath;
+            this._targetConnectionString = targetConnectionString;
+        }
+
+        public void Execute()
+        {
+            using (var reader = new StreamReader(_sourceFilePath))
+            {
+                var line = reader.ReadLine();
+                var headers = line.Split(',').ToArray();
+
+                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ETL"].ConnectionString))
+                {
+                    connection.Open();
+                    var transaction = connection.BeginTransaction();
+
+                    try
+                    {
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            var values = line.Split(',').ToArray();
+
+                            using (var cmd = connection.CreateCommand())
+                            {
+                                cmd.CommandText = "INSERT INTO Accounts (Number, Name) VALUES (@number, @name)";
+                                cmd.Transaction = transaction;
+                                cmd.Parameters.AddWithValue("@number", values[0]);
+                                cmd.Parameters.AddWithValue("@name", values[1]);
+                                cmd.ExecuteNonQuery();
+                            }
+                        }
+
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
--- Src/SOLID/SOLID.ETL/Program.cs
+++ Src/SOLID/SOLID.ETL/Program.cs
@@ -10,43 +10,10 @@ namespace SOLID.ETL
     {
         static void Main(string[] args)
         {
-            var filePath = args[0];
+            var sourceFilePath = args[0];
+            var targetConnectionString = ConfigurationManager.ConnectionStrings["ETL"].ConnectionString;
 
-            using (var reader = new StreamReader(filePath))
-            {
-                var line = reader.ReadLine();
-                var headers = line.Split(',').ToArray();
-
-                using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ETL"].ConnectionString))
-                {
-                    connection.Open();
-                    var transaction = connection.BeginTransaction();
-
-                    try
-                    {
-                        while ((line = reader.ReadLine()) != null)
-                        {
-                            var values = line.Split(',').ToArray();
-
-                            using (var cmd = connection.CreateCommand())
-                            {
-                                cmd.CommandText = "INSERT INTO Accounts (Number, Name) VALUES (@number, @name)";
-                                cmd.Transaction = transaction;
-                                cmd.Parameters.AddWithValue("@number", values[0]);
-                                cmd.Parameters.AddWithValue("@name", values[1]);
-                                cmd.ExecuteNonQuery();
-                            }
-                        }
-
-                        transaction.Commit();
-                    }
-                    catch (Exception)
-                    {
-                        transaction.Rollback();
-                        throw;
-                    }
-                }
-            }
+            new EtlProcessor(sourceFilePath, targetConnectionString).Execute();
         }
     }
 }
